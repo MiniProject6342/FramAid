25c956a848b64bba1f85e02b64f6adcdd60acc7c
MiniProject6342 <132653522+MiniProject6342@users.noreply.github.com>
2023-07-26 22:01:18 +0530

    Create Welcome.java

---
+++ Servlets/Welcome.java
@@ -0,0 +1,57 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Welcome
+ */
+@WebServlet("/Welcome")
+public class Welcome extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Welcome() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession hs=request.getSession(false);
+		PrintWriter pw=response.getWriter();
+		if(hs!=null) {
+			pw.print("welcome "+hs.getAttribute("username"));
+			pw.println("<br> do you want to logout?");
+			pw.print("<br><form action='Logout' method='get'><button type='submit'>yes</button><br></form>");
+		}
+		else {
+			pw.print("please login");
+			RequestDispatcher rd=request.getRequestDispatcher("index.html");
+			rd.include(request, response);
+		}
+	}
+
+}
